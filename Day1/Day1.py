ad82a5a3d9dbefe5d0ec337d8f0106f003c46723
kaiken1987 <kaiken1987@gmail.com>
2025-12-01 11:42:40 -0500

    Day 1 Part 1

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,15 +1,29 @@
 import math
 
-f = open("example.txt", "r")
+#f = open("example.txt", "r")
 
-#f = open("input.txt", "r")
+f = open("input.txt", "r")
 
 
 def part1():
 	print( "Part 1")
-		#print( f"{i}: {stones}\n" )
-
-	#print(f"Total stones: {len(stones)}")
+	dial = 50
+	zeros = 0
+	
+	for line in f:
+		change = int(line[1:-1])
+		if (line[0] == 'R' ): #Right -> add
+			dial += change
+		elif(line[0] == 'L') :
+			dial -= change
+		else:
+			break
+		dial = dial%100
+		if( dial == 0 ):
+			zeros+=1
+		
+		
+	print(f"Password is: {zeros}")
 
 
 def part2():
