35c74c9b7f667e569eb7642d965ba67e1a98ddbd
kaiken1987 <kaiken1987@gmail.com>
2025-12-01 11:32:47 -0500

    Initialized git repo for 12 days of AoC

---
+++ Day12/Day12.py
@@ -0,0 +1,22 @@
+﻿import math
+
+f = open("example.txt", "r")
+
+#f = open("input.txt", "r")
+
+
+def part1():
+	print( "Part 1")
+		#print( f"{i}: {stones}\n" )
+
+	#print(f"Total stones: {len(stones)}")
+
+
+def part2():
+	print( "Part 2")
+	#print(f"Total stones: {sum}")
+	
+part1()
+part2()
+	 
+
